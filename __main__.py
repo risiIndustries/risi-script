812c5ba6e1d1fcfca65b1c9ba23c18860509e8a0
pizzalovingnerd <pizzalovingnerd@teknik.io>
2021-12-13 00:04:26 -0800

    Redoing with YAML instead of custom syntax. This helps us save time on parsing, documenting, and reduces the learning curve to develop with.

--- __main__.py
+++ __main__.py
@@ -1,54 +1,88 @@
 import yaml
 import tempfile
-import os
 import subprocess
 
-# Input functions
-simplefunctions = {}
-
-simplefunctions["@INPUT"] = """
-echo "@INPUT Ran"
-"""
-simplefunctions["@FILE"] = """
-echo "@FILE Ran"
-"""
-simplefunctions["@DIRECTORY"] = """
-echo "@DIRECTORY Ran"
-"""
-simplefunctions["@CHOICE"] = """
-echo "@CHOICE Ran"
-"""
-simplefunctions["@CHECKFORFILE"] = """
-python3 bashinterface.py CHECKFORFILE
-"""
-simplefunctions["@CHECKINFILE"] = """
-echo "@CHECKINFILE Ran"
-"""
-simplefunctions["@CHECKOUTPUT"] = """
-echo "@CHECKOUTPUT Ran"
-"""
-simplefunctions["@CHECKOUTPUTLINE"] = """
-echo "@CHECKOUTPUTLINE Ran"
-"""
-simplefunctions["@CHECKPACKAGE"] = """
-echo "@CHECKPACKAGE Ran"
+test = """
+metadata:
+  name: "ScriptName"
+  description: "Description of what the script does"
+  dependencies:
+    - package1
+    - package2
+  root: true
+  risiscript_version: 1
+  
+run: # Conflicts with install function
+  bash: |
+    echo "hello world" |
+  checks:
+  - CHECKFORFILE:
+    - file
+  - CHECKINFILE:
+    - file
+    - "string"
+  - CHECKOUTPUT:
+    - ["bash", "command"] # Subprocess List for Python
+    - "output"
+  - CHECKOUTPUTLINE:
+    - ["bash", "command"]
+    - "output"
 """
 
-complexfunctions = {}
 
-def compile_script(file):
-    temp_file_path = tempfile.mkstemp()[1]
-    new_file = open(temp_file_path, "w")
+class RisiScriptError(Exception):
+    """Raised when something is wrong with your risi script code"""
+    pass
 
-    file_list = file.read().splitlines()
-    while "" in file_list:
-        file_list.remove("")
 
-    for line in file_list:
-        line_list = line.split()
-        if line_list[0] in simplefunctions.keys():
-            new_file.write(simplefunctions[line_list[0]].strip() + "\n")
-        else:
-            new_file.write(line + "\n")
+class Metadata:
+    def __init__(self, metadata):
+        for key in metadata:
+            setattr(self, key, metadata[key])
+        if "none" in str(self.dependencies).lower() or "null" in str(self.dependencies).lower():
+            self.dependencies = None
 
-def parse(code):
+
+class Script:
+    def __init__(self, code):
+        parsed_code = yaml.safe_load(code)
+        self.metadata = Metadata(parsed_code["metadata"])
+
+def syntax_check(parsed_code):
+    # Checking Metadata
+    if "metadata" not in parsed_code.keys():
+        raise RisiScriptError("file must contain metadata function")
+    elif "name" not in parsed_code["metadata"].keys():
+        raise RisiScriptError("script name missing from metadata")
+    elif "description" not in parsed_code["metadata"].keys():
+        raise RisiScriptError("description missing from metadata")
+    elif "dependencies" not in parsed_code["metadata"].keys():
+        raise RisiScriptError("dependencies missing from metadata, add none for no dependencies")
+    elif "root" not in parsed_code["metadata"].keys():
+        raise RisiScriptError("root missing from metadata")
+    elif "risiscript_version" not in parsed_code["metadata"].keys():
+        raise RisiScriptError("risiscript version missing from metadata")
+
+    # Checking for conflicting functions
+    elif "run" in parsed_code.keys() and "install" in parsed_code.keys():
+        raise RisiScriptError("file cannot contain the run and install functions")
+    elif "install" in parsed_code.keys() and "remove" not in parsed_code.keys():
+        raise RisiScriptError("update function detected without install function")
+    elif "update" in parsed_code.keys() and "install" not in parsed_code.keys():
+        raise RisiScriptError("update function detected without install function")
+
+    # Checking for bash and checks options
+    for item in ["run", "install", "update", "remove"]:
+        if item in parsed_code.keys():
+            if "bash" not in parsed_code[item]:
+                raise RisiScriptError("bash code missing from {} function".format(item))
+            if "checks" not in parsed_code[item]:
+                raise RisiScriptError("checks missing from {} function".format(item))
+
+
+# name: "Test Script"
+# description: "This is a test"
+# dependencies:
+# - test_dep
+# root: false
+# risiscript_version: 1
